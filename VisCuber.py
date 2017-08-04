ec86ba720948f084785999d83e7b733f12c2d961
Jason MacDonald <jsmacdonald@gmail.com>
2017-08-04 11:13:53 -0700

    Added Setcode Parsing
    I added a set code parser to the VisCuber.py.  If you have the correct
    setcode of the card you are attempting to load an image from in
    gatherer, then you’ll get a image source that points to the correct
    card image of your card now.

--- VisCuber.py
+++ VisCuber.py
@@ -1,9 +1,10 @@
 ## This is a super basic python code that will read in a list of cards and convert each card into it's own visual cube html call.
 
 # Create the html string that will show the card, sans the card's name.  This will be in 4 parts.
-String1 = '<li data-checked="true"><a href="http://gatherer.wizards.com/Pages/Card/Details.aspx?name="'
+String1 = '<li data-checked="true"><a href="http://gatherer.wizards.com/Pages/Card/Details.aspx?name='
 String2 = '"><img src="http://gatherer.wizards.com/Handlers/Image.ashx?name='
-String3 = '&amp;set=&amp;type=card" alt="'
+String3 = '&amp;set='
+String4 = '&amp;type=card" alt="'
 StringEnd = '"></a></li>\n'
 
 # Now let's open the input file and read the contents, then create the output
@@ -15,7 +16,17 @@ OutputFile = open(OutputFN, 'w')
 #print InputFile.read()
 for card in InputFile:
     #print card
-    card1 = card.strip()
+
+    #First let's split out the set code
+    card0=card.split('(')
+    if len(card0)>1:
+        setcode = card0[1].strip()
+        setcode = setcode[:-1]
+        #print setcode
+    else:
+        setcode=''
+    
+    card1 = card0[0].strip()
     cardhtml = card1
     ## Replace commas
     cardhtml=cardhtml.replace(",","%2C")
@@ -24,7 +35,7 @@ for card in InputFile:
     ## Replace Spaces
     cardhtml=cardhtml.replace(" ","%20")
     #print cardhtml
-    htmlString = String1 + cardhtml + String2 + cardhtml + String3 + card1 + StringEnd
+    htmlString = String1 + cardhtml + String2 + cardhtml + String3 + setcode + String4 + card1 + StringEnd
     OutputFile.write(htmlString)
 
 InputFile.close()
